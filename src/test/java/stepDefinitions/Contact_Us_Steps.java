2ec60e1de367dc0e4028aedcaa155a82e835fef6
GregHajduk <70976488+GregHajduk@users.noreply.github.com>
2023-03-05 22:50:44 +0000

    created DriverFactory

--- src/test/java/stepDefinitions/Contact_Us_Steps.java
+++ src/test/java/stepDefinitions/Contact_Us_Steps.java
@@ -1,37 +1,20 @@
 package stepDefinitions;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
 import org.testng.Assert;
 
+import static driver.DriverFactory.getDriver;
+
 public class Contact_Us_Steps {
-    private WebDriver driver;
-
-    @Before("@contact-us")
-    public void setup() {
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/java/drivers/chromedriver");
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-        this.driver = new ChromeDriver(chromeOptions);
-        this.driver.manage().window().maximize();
-    }
-
-    @After("@contact-us")
-    public void tearDown() {
-        this.driver.quit();
-    }
-
+    private WebDriver driver = getDriver();
     public String generateRandomNumber(int length) {
         return RandomStringUtils.randomAlphabetic(length);
     }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -1,14 +1,15 @@
+@login @regression
+
 Feature: WebDriver University - Login Page
 
-  @login
-  Scenario Outline: Successful login with valid credentials
+  Scenario Outline: Successful & Unsuccessful Login check
     When the user accesses the webdriver university login page
     And enters valid username <username>
     And enters valid password <password>
     And clicks on the login button
     Then user should be presented with the successful login popup message "<validationMessage>"
     Examples:
-      | username  | password     | validationMessage   |
+      | username  | password     | validationMessage    |
       | webdriver | webdriver123 | validation succeeded |
       | webdriver | password777  | validation failed    |
       | driver    | webdriver123 | validation failed    |
