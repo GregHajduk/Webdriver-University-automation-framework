23b1aea6ab127d61275d20199a5a97c07eec5543
GregHajduk <70976488+GregHajduk@users.noreply.github.com>
2023-03-06 16:16:09 +0000

    added attaching images to reports

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -6,7 +6,7 @@ import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
         tags = "@regression", monochrome = true, dryRun = false,
-        plugin = {"pretty", "html:target/cucumber.html"})
+        plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"})
 public class MainRunner extends AbstractTestNGCucumberTests {
     @Override
     @DataProvider(parallel = true)
