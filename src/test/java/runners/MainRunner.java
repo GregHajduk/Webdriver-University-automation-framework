6db14532c21f5be9ff0ce1a60b35d05667e0c328
GregHajduk <70976488+GregHajduk@users.noreply.github.com>
2023-03-06 09:52:03 +0000

    adder cucumber parallel execution

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -2,9 +2,15 @@ package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
         tags = "@regression", monochrome = true, dryRun = false,
-        plugin = {"pretty", "json:target/cucumber.json"})
+        plugin = {"pretty", "html:target/cucumber.html"})
 public class MainRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
